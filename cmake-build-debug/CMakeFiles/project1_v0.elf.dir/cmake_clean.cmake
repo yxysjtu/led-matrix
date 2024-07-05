2f1e09c5eb6017b3060602a81ce469b62bff53a9
yxysjtu <1137088520@qq.com>
2024-07-05 16:21:39 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project1_v0.elf.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/main.cpp.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/stm32g0xx_it.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Src/system_stm32g0xx.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Core/Startup/startup_stm32g030k8tx.s.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_spi_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj"
+  "CMakeFiles/project1_v0.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj"
+  "project1_v0.elf"
+  "project1_v0.elf.manifest"
+  "project1_v0.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C CXX)
+  include(CMakeFiles/project1_v0.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project1_v0.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project1_v0.elf.
+# This may be replaced when dependencies are built.
